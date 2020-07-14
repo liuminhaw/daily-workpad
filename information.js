b818582476ddeb668a4e42e04165ee4ea99bdd69
Min-Haw, Liu <liuminhaw@gmail.com>
2020-07-14 09:15:30 +0800

    update: get today's row and workpad content

--- information.js
+++ information.js
@@ -3,6 +3,7 @@ function Pad() {
 
     this.year = currentDate.getFullYear();
     this.month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+    this.date = ("0" + currentDate.getDate()).slice(-2);
     this.sheetName = this.year + "/" + this.month;
 
     this.sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this.sheetName);
@@ -15,8 +16,32 @@ function Pad() {
         Logger.log("Name: " + this.sheetName);
     }
 
-    // TODOs: getDateRow method
+    // Get current date row number
+    function getDateRow(sheet) {
+        var now = new Date();
+        var values = sheet.getRange("A:A").getValues();
+        Logger.log("Length of values: " + values.length);
+        // Logger.log(values);
 
+        for (var i = 1; i < values.length; i++ ) {
+            if (_sameDate(now, values[i][0])) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+    this.currentDateRow = getDateRow(this.sheet);
+
+    // Get current date pad content
+    this.contents = this.sheet.getRange(this.currentDateRow, 2, 1, 5).getValues();
+
+    function _sameDate(d1, d2) {
+        return (d1.getDate() === d2.getDate() 
+                && d1.getMonth() === d2.getMonth()
+                && d1.getFullYear() === d2.getFullYear())
+    }
+
+    
     // TODOs: updatePad method
 }
 
@@ -25,10 +50,15 @@ function getSheet() {
         var workpad = new Pad();
     }
     catch (e) {
+        Logger.log(e.message);
         Logger.log("Sheet not found");
         return 1;
     }
 
     workpad.showName();
+    Logger.log("Current date row " + workpad.currentDateRow);
+    Logger.log("Check row value " + workpad.sheet.getRange(workpad.currentDateRow, 1).getValue());
+    Logger.log("Today's Pad: " + workpad.contents);
+
 }
 
--- update.js
+++ update.js
@@ -1,19 +1,20 @@
+// Function reference to test content strikethrough
 function tester() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  Logger.log(sheet.getName());
-  var cell = sheet.getRange("C9");
-  //Logger.log(cell.getValue());
-  var richText = cell.getRichTextValue();
-  var runs = richText.getRuns()
-  Logger.log("length: " + runs.length);
-  
-  for (var i = 0; i < runs.length; i++) {
-    Logger.log("index: " + i);
-    Logger.log(runs[i].getText());
-    Logger.log(richText.getRuns()[i].getTextStyle().isStrikethrough());
-  }
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    Logger.log(sheet.getName());
+    var cell = sheet.getRange("C9");
+    //Logger.log(cell.getValue());
+    var richText = cell.getRichTextValue();
+    var runs = richText.getRuns()
+    Logger.log("length: " + runs.length);
+    
+    for (var i = 0; i < runs.length; i++) {
+        Logger.log("index: " + i);
+        Logger.log(runs[i].getText());
+        Logger.log(richText.getRuns()[i].getTextStyle().isStrikethrough());
+    }
 
-  Logger.log("Test clasp");
+    Logger.log("Test clasp");
 
 }
 
